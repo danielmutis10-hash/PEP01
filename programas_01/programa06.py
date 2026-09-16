4171c583b10cbd096747b9361da7313490a8c7a1
danielmutis10-hash <daniel.mutis10@gmail.com>
2026-09-16 22:34:46 +0200

    cambios en el programa 06

--- programas_01/programa06.py
+++ programas_01/programa06.py
@@ -12,4 +12,20 @@ print(f"{a} % {b} = {a%b}")
 print(f"{a} ** {b} = {a**b}")
 
 #Operadores lógicos
+print("\n------Operadores Lógicos-----")
+print(f"{x} y {y} -> {x and y}")
+print(f"{x} o {y} -> {x or y}")
+print(f"Negativo de {x} -> {not x}")
+print(f"Negativo de {y} -> {not y}")
+
+#Operadores de comparación con números y booleanos
+print("\n-----Operadores comparativos-----")
+print(f"{a} == {b} -> {a == b}")
+print(f"{a} != {b} -> {a != b}")
+print(f"{a} > {b} -> {a > b}")
+print(f"{a} < {b} -> {a < b}")
+print(f"{a} >= {b} -> {a >= b}")
+print(f"{b} >= {a} -> {b >= a}")
+print(f"{x} == {y} -> {x == y}")
+print(f"{y} == {x} -> {y == x}")
 
