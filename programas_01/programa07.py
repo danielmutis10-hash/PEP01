6376793784c2bcd2479007d8cd10a8489d1ed31c
danielmutis10-hash <daniel.mutis10@gmail.com>
2026-09-16 23:26:08 +0200

    Agregar programa 07

---
+++ programas_01/programa07.py
@@ -0,0 +1,21 @@
+'''
+Creación de programa para la escritura de 34 en binario, octal y hexadecimal
+además de los números 0b1001, 0o12, 0xa3f en python y con print
+'''
+#Todos en una misma linea
+print( 0b100010 , 0o42, 0x22 )
+
+#Cada uno por separado
+print( 0b100010)
+print( 0o42)
+print( 0x22)
+
+'''Ahora el escribir los números anteriores.
+Primero todos juntos y luego separados'''
+#Todos juntos
+print(0b1001, 0o12, 0xa3f)
+
+#Cada uno por separado
+print(0b1001)
+print(0o12)
+print(0xa3f)
